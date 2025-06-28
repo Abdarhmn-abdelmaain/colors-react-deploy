a1f240bb166554de90853b66cf5cec5f1db63089
abdalrhmn <abdalrhmnabdelmaain@gmail.com>
2025-06-28 11:02:52 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function App() {
+  const [colors ,setColors] = useState([])
+
+  const handle = () =>{
+    const oldColor = [...colors]
+    oldColor.push('#' + Math.trunc(Math.random() * 10000))
+    setColors(oldColor)
+  }
+  return (
+    <div className='mum'>
+      <header className='text-center text-primary col-12'><h2>Welcome to the world of colors</h2></header>
+      <button className=' text-danger bg-black col-2 fs-5' onClick={handle}>Add color</button>
+      {colors.map((el,index) => {return(
+        <button key={index} style={{backgroundColor:el}}>{el}</button>
+      )})}
+
+    </div>
+  )
+}
+
+
