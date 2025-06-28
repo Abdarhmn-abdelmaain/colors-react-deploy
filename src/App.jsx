11e4d803b1f258adbaa04bb608c6bb22146d16f0
abdalrhmn <abdalrhmnabdelmaain@gmail.com>
2025-06-28 11:31:19 +0300

    new app

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className='mum'>
       <header className='text-center text-primary col-12'><h2>Welcome to the world of colors</h2></header>
-      <button className=' text-danger bg-primary fs-5' style={{width:'100px'}} onClick={handle}>Add color</button>
+      <button className=' text-danger bg-black fs-5' style={{width:'200px'}} onClick={handle}>Add color</button>
       {colors.map((el,index) => {return(
         <button key={index} style={{backgroundColor:el}}>{el}</button>
       )})}
